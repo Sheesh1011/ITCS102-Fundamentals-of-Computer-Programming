1c8ab737f3c063ca544fc842ecd7fe87eb50f36c
Lythain <kaiyle1101@gmail.com>
2025-08-11 19:11:48 +0800

    Initial Commit

---
+++ code_challenge1.py
@@ -0,0 +1,7 @@
+name = input("Type your name -->")
+
+#print("\t\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t*\t\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t*\t\t\tHi",name,"\t\t\t\t*\n\t\t*\t\t\t\t\t\t*\n\t\t\t*\t\t\t\t*\n\t\t\t\t*\t\t*\n\t\t\t\t\t*")
+
+#print("\t\t\t\t\t*\n\n\t\t\t\t*\t\t*\n\n\t\t\t*\t\t\t\t*\n\n\t\t*\t\t\t\t\t\t*\n\n\t*\t\t\tHi",name,"\t\t\t\t*\n\n\t\t*\t\t\t\t\t\t*\n\n\t\t\t*\t\t\t\t*\n\n\t\t\t\t*\t\t*\n\n\t\t\t\t\t*")
+
+print("\t\t\t\t\t*\n\n\n\t\t\t\t*\t\t*\n\n\n\t\t\t*\t\t\t\t*\n\n\n\t\t*\t\t\t\t\t\t*\n\n\n\t*\t\t\tHi",name,"\t\t\t\t*\n\n\n\t\t*\t\t\t\t\t\t*\n\n\n\t\t\t*\t\t\t\t*\n\n\n\t\t\t\t*\t\t*\n\n\n\t\t\t\t\t*")
