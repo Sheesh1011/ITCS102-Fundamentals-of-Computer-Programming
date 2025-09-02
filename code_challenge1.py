9bace06a06197aa5b9f1a4fa3da33812c4bd3483
Sheesh1011 <kaiyle1101@gmail.com>
2025-09-03 07:41:09 +0800

    Update code_challenge1.py

--- code_challenge1.py
+++ code_challenge1.py
@@ -4,6 +4,4 @@ name = input("Type your name -->")
 
 #print("\t\t\t\t\t*\n\n\t\t\t\t*\t\t*\n\n\t\t\t*\t\t\t\t*\n\n\t\t*\t\t\t\t\t\t*\n\n\t*\t\t\tHi",name,"\t\t\t\t*\n\n\t\t*\t\t\t\t\t\t*\n\n\t\t\t*\t\t\t\t*\n\n\t\t\t\t*\t\t*\n\n\t\t\t\t\t*")
 
-#print("\t\t\t\t\t*\n\n\n\t\t\t\t*\t\t*\n\n\n\t\t\t*\t\t\t\t*\n\n\n\t\t*\t\t\t\t\t\t*\n\n\n\t*\t\t\tHi",name,"\t\t\t\t*\n\n\n\t\t*\t\t\t\t\t\t*\n\n\n\t\t\t*\t\t\t\t*\n\n\n\t\t\t\t*\t\t*\n\n\n\t\t\t\t\t*")
-
-print("\t\t\t\t\t\t\t*\n\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t*\t\tHi\t\t\t\t*\n\t\t\t*\t\t\t\tAfuang\t\t\t\t*\n\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t*")
+print("\t\t\t\t\t*\n\n\n\t\t\t\t*\t\t*\n\n\n\t\t\t*\t\t\t\t*\n\n\n\t\t*\t\t\t\t\t\t*\n\n\n\t*\t\t\tHi",name,"\t\t\t\t*\n\n\n\t\t*\t\t\t\t\t\t*\n\n\n\t\t\t*\t\t\t\t*\n\n\n\t\t\t\t*\t\t*\n\n\n\t\t\t\t\t*")
