97b4e3a839bd9e284a8582b5826097dafae09b7c
Lythain <kaiyle1101@gmail.com>
2025-09-10 20:32:05 +0800

    Commit Activity

---
+++ activity11_temp.py
@@ -0,0 +1,22 @@
+temp = input("What is the temperature oustide today? ")
+
+if int(temp.isnumeric()):
+    if temp <= 0:
+        print("The temperature is BELOW FREEZING today")
+    elif temp >= 1 and temp <= 15:
+        print("The temperature is EXTREMELY COLD today")
+    elif temp >= 16 and temp <= 30:
+        print("The temperature is COLD today")
+    elif temp >= 31 and temp <= 38:
+        print("The temperature is LUKEWARM today")
+    elif temp >= 39 and temp <= 42:
+        print("The temperature is WARM today")
+    elif temp >= 43 and temp <= 50:
+        print("The temperature is HOT today")
+    elif temp >= 51 and temp <= 60:
+        print("The temperature is EXTREMELY HOT today")
+    elif temp >= 61:
+        print("The temperature is BURNING HOT today")
+
+else:
+    print("ERROR: Invalid Temperature")
